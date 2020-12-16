a3d3f7c8777f3a80ef22fed5ab2d936e59f6e05d
Mehdi Neysi <dev.mehdineysi@gmail.com>
2020-12-16 17:57:29 +0330

    Feat: custom scrollbar like mac os

--- src/components/AddPerson.jsx
+++ src/components/AddPerson.jsx
@@ -4,27 +4,27 @@ import { IconButton, Input, InputGroup } from '@chakra-ui/react'
 import { BiPlus } from 'react-icons/bi'
 
 const AddPerson = ({ addPerson, setPerson, personName }) => {
-    return (
-        <form onSubmit={addPerson}>
-            <InputGroup size="lg" mt="1.2rem">
-                <Input
-                    id="add-person"
-                    variant="outline"
-                    placeholder="Add new person ..."
-                    onChange={setPerson}
-                    value={personName}
-                />
-                <IconButton
-                    as="button"
-                    type="submit"
-                    ml="8px"
-                    aria-label="Add New Person"
-                    colorScheme="green"
-                    icon={<BiPlus size="1.2rem" />}
-                />
-            </InputGroup>
-        </form>
-    )
+	return (
+		<form onSubmit={addPerson}>
+			<InputGroup size="lg" mt="1.2rem">
+				<Input
+					id="add-person"
+					variant="outline"
+					placeholder="Add new person ..."
+					onChange={setPerson}
+					value={personName}
+				/>
+				<IconButton
+					as="button"
+					type="submit"
+					ml="8px"
+					aria-label="Add New Person"
+					colorScheme="green"
+					icon={<BiPlus size="1.2rem" />}
+				/>
+			</InputGroup>
+		</form>
+	)
 }
 
 export default AddPerson
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,126 +1,140 @@
 import React, { Component } from 'react'
 
 import {
-    Alert,
-    AlertIcon,
-    Button,
-    ChakraProvider,
-    Container,
+	Alert,
+	AlertIcon,
+	Button,
+	ChakraProvider,
+	Container,
 } from '@chakra-ui/react'
 import Swal from 'sweetalert2'
+import SimpleBar from 'simplebar-react'
+import Scrollbar from 'smooth-scrollbar'
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll'
+
+import 'simplebar/dist/simplebar.min.css'
 
 import AddPerson from '../components/AddPerson'
 import Header from '../components/Header'
 import Persons from '../components/Persons'
 
 class App extends Component {
-    state = {
-        persons: [],
-        person: '',
-        showPersons: true,
-    }
+	state = {
+		persons: [],
+		person: '',
+		showPersons: true,
+	}
 
-    handleShowPersons = () => {
-        this.setState({ showPersons: !this.state.showPersons })
-    }
+	handleShowPersons = () => {
+		this.setState({ showPersons: !this.state.showPersons })
+	}
 
-    handleDeletePerson = id => {
-        const persons = [...this.state.persons]
+	handleDeletePerson = id => {
+		const persons = [...this.state.persons]
 
-        const personName = persons.filter(p => p.id === id)[0].fullName
-        const filteredPersons = persons.filter(p => p.id !== id)
+		const personName = persons.filter(p => p.id === id)[0].fullName
+		const filteredPersons = persons.filter(p => p.id !== id)
 
-        Swal.fire({
-            icon: 'error',
-            title: 'Delete Person',
-            text: `Are you sure you want to delete "${personName}"?`,
-            showCancelButton: true,
-            confirmButtonText: 'Delete',
-            confirmButtonColor: '#e41b1b'
-        }).then(result => {
-            if (result.isConfirmed) {
-                this.setState({ persons: filteredPersons })
-            }
-        })
-    }
+		Swal.fire({
+			icon: 'error',
+			title: 'Delete Person',
+			text: `Are you sure you want to delete "${personName}"?`,
+			showCancelButton: true,
+			confirmButtonText: 'Delete',
+			confirmButtonColor: '#e41b1b',
+		}).then(result => {
+			if (result.isConfirmed) {
+				this.setState({ persons: filteredPersons })
+			}
+		})
+	}
 
-    handleNameChange = (event, id) => {
-        const { persons: allPersons } = this.state
+	handleNameChange = (event, id) => {
+		const { persons: allPersons } = this.state
 
-        const personIndex = allPersons.findIndex(p => p.id === id)
-        const person = allPersons[personIndex]
-        person.fullName = event.target.value
+		const personIndex = allPersons.findIndex(p => p.id === id)
+		const person = allPersons[personIndex]
+		person.fullName = event.target.value
 
-        const persons = [...this.state.persons]
-        persons[personIndex] = person
-        this.setState({ persons })
-    }
+		const persons = [...this.state.persons]
+		persons[personIndex] = person
+		this.setState({ persons })
+	}
 
-    handleAddPerson = event => {
-        event.preventDefault()
-        if (this.state.person.trim().length === 0) {
-            return Swal.fire({
-                icon: 'warning',
-                title: 'Warning',
-                text: 'Please enter a person name.'
-            })
-        }
+	handleAddPerson = event => {
+		event.preventDefault()
+		if (this.state.person.trim().length === 0) {
+			return Swal.fire({
+				icon: 'warning',
+				title: 'Warning',
+				text: 'Please enter a person name.',
+			})
+		}
 
-        const persons = [...this.state.persons]
-        const person = { id: persons.length + 1, fullName: this.state.person }
+		const persons = [...this.state.persons]
+		const person = { id: persons.length + 1, fullName: this.state.person }
 
-        persons.push(person)
-        this.setState({ persons, person: '' })
-    }
+		persons.push(person)
+		this.setState({ persons, person: '' })
+	}
 
-    setPerson = event => {
-        this.setState({ person: event.target.value })
-    }
+	setPerson = event => {
+		this.setState({ person: event.target.value })
+	}
 
-    render() {
-        const { persons, person, showPersons } = this.state
+	render() {
+		const { persons, person, showPersons } = this.state
 
-        window.onload = () => {
-            document.querySelector('#add-person').focus()
-        }
+		window.onload = () => {
+			document.querySelector('#add-person').focus()
 
-        return (
-            <ChakraProvider>
-                <Header length={persons.length} />
-                <Container>
-                    <Button
-                        disabled={persons.length === 0}
-                        colorScheme="blue"
-                        size="md"
-                        onClick={this.handleShowPersons}
-                    >
-                        {showPersons ? 'Hide Persons' : 'Show Persons'}
-                    </Button>
+			Scrollbar.use(OverscrollPlugin)
+			Scrollbar.init(document.querySelector('div[data-simplebar="init"]'), {
+				plugins: {
+					overscroll: true,
+				},
+			})
+		}
 
-                    <AddPerson
-                        addPerson={this.handleAddPerson}
-                        setPerson={this.setPerson}
-                        personName={person}
-                    />
+		return (
+			<SimpleBar style={{ maxHeight: '100vh', textAlign: 'center' }}>
+				<ChakraProvider>
+					<Header length={persons.length} />
+					<Container>
+						<Button
+							disabled={persons.length === 0}
+							colorScheme="blue"
+							size="md"
+							onClick={this.handleShowPersons}
+						>
+							{showPersons ? 'Hide Persons' : 'Show Persons'}
+						</Button>
 
-                    {showPersons ? (
-                        <Persons
-                            persons={persons}
-                            deletePerson={this.handleDeletePerson}
-                            changePerson={this.handleNameChange}
-                        />
-                    ) : null}
+						<AddPerson
+							addPerson={this.handleAddPerson}
+							setPerson={this.setPerson}
+							personName={person}
+						/>
 
-                    {persons.length === 0 ? (
-                        <Alert mt="1.2rem" rounded="md" status="warning">
-                            <AlertIcon />
-                            There is no name, you can add one above.
-                        </Alert>
-                    ) : null}
-                </Container>
-            </ChakraProvider>
-        )
-    }
+						{showPersons ? (
+							<Persons
+								persons={persons}
+								deletePerson={this.handleDeletePerson}
+								changePerson={this.handleNameChange}
+							/>
+						) : null}
+
+						{persons.length === 0 ? (
+							<Alert mt="1.2rem" rounded="md" status="warning">
+								<AlertIcon />
+								There is no name, you can add one above.
+							</Alert>
+						) : null}
+					</Container>
+				</ChakraProvider>
+			</SimpleBar>
+		)
+	}
 }
 
 export default App
