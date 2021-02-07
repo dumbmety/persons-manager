7a4f309f85ea9c2b9e9497553aea16bf0f3d3221
Mehdi Neysi <68688806+neysidev@users.noreply.github.com>
2021-02-07 21:39:17 +0330

    Update: remove extra spaces

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './styles/index.css';
-
 import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
